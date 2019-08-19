0a6c6ab94e9ddd5a4c222041595e746a942b7f2b
Devyn Gish <devyngish@gmail.com>
2019-08-19 15:34:55 -0600

    yomama

---
+++ app/models/post_itss.rb
@@ -0,0 +1,2 @@
+class PostItss < ApplicationRecord
+end
---
+++ test/models/post_itss_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PostItssTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

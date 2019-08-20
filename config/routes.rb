8c096b1242af549aa06515167bfc0c84179b05b0
Devyn Gish <devyngish@gmail.com>
2019-08-19 21:39:49 -0600

    finally it worksgit add .

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'static_notes#home'
-  get'/about', to: 'static_notes#about'
+
+  root 'static_pages#home'
+  get'/about', to: 'static_pages#about'
 
   resources :notes
 end
